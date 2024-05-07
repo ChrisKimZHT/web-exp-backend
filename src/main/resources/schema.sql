569eaa56fb12d9d2eb61c1f7035a3f345baa2644
ChrisKim <1515748548@qq.com>
2024-05-07 22:14:06 +0800

    [fix] email要unique

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user
 (
     user_id          INT PRIMARY KEY AUTO_INCREMENT,
-    email            VARCHAR(255) NOT NULL,
+    email            VARCHAR(255) NOT NULL UNIQUE,
     hashed_password  VARCHAR(255) NOT NULL,
     avatar_file_name VARCHAR(255)
 );
