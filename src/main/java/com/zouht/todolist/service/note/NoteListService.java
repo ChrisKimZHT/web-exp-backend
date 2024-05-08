4714e7cb7258818dca6b8c5ec91b34cfb459dada
ChrisKim <1515748548@qq.com>
2024-05-08 16:30:48 +0800

    [fix] 用户只能看自己的内容

--- src/main/java/com/zouht/todolist/service/note/NoteListService.java
+++ src/main/java/com/zouht/todolist/service/note/NoteListService.java
@@ -1,7 +1,11 @@
 package com.zouht.todolist.service.note;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zouht.todolist.mapper.NoteMapper;
 import com.zouht.todolist.pojo.Note;
+import com.zouht.todolist.service.user.UserDetailImpl;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,7 +18,14 @@ public class NoteListService {
     NoteMapper noteMapper;
 
     public Map<String, Object> list() {
-        List<Note> noteList = noteMapper.selectList(null);
+        UsernamePasswordAuthenticationToken authenticationToken = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailImpl loginUSer = (UserDetailImpl) authenticationToken.getPrincipal();
+        Integer user = loginUSer.getUser().getUserId();
+
+        QueryWrapper<Note> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", user);
+        List<Note> noteList = noteMapper.selectList(queryWrapper);
+
         return Map.of("status", 0, "message", "OK", "data", noteList);
     }
 }
--- src/main/java/com/zouht/todolist/service/todo/TodoListService.java
+++ src/main/java/com/zouht/todolist/service/todo/TodoListService.java
@@ -1,7 +1,11 @@
 package com.zouht.todolist.service.todo;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zouht.todolist.mapper.TodoMapper;
 import com.zouht.todolist.pojo.Todo;
+import com.zouht.todolist.service.user.UserDetailImpl;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,7 +18,14 @@ public class TodoListService {
     TodoMapper todoMapper;
 
     public Map<String, Object> list() {
-        List<Todo> todoList = todoMapper.selectList(null);
+        UsernamePasswordAuthenticationToken authenticationToken = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailImpl loginUSer = (UserDetailImpl) authenticationToken.getPrincipal();
+        Integer user = loginUSer.getUser().getUserId();
+
+        QueryWrapper<Todo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", user);
+        List<Todo> todoList = todoMapper.selectList(queryWrapper);
+
         return Map.of("status", 0, "message", "OK", "data", todoList);
     }
 }
